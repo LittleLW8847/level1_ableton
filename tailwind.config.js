27df77d2dc178098eb2c95ef36b73ee65348a30f
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2024-07-24 22:45:18 +0700

    try to add new flex size, but failed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      Flex:{
+        '1/10': 10%rem,
+      }
+    },
   },
   plugins: [],
 }
