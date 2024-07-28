185739df8fb832cf76d5362cf738fdecfd88958e
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2024-07-28 23:51:07 +0700

    navbar 2 layout complete!!!!

--- src/compnent/Navbar2.js
+++ src/compnent/Navbar2.js
@@ -1,10 +1,10 @@
 export default function Navbar2(){
     return(
         <nav class="flex flex-row">
-            <ul class="flex flex-row">
-                <li>About</li>
-                <li>Jobs</li>
-                <li>Apprenticeship</li>
+            <ul class="flex flex-row mx-10">
+                <li class="basis-10 mr-2">About</li>
+                <li class="basis-10 mx-3.5">Jobs</li>
+                <li class="basis-10 ml-2">Apprenticeship</li>
             </ul>
         </nav>
     );
