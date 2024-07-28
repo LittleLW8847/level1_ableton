69ccb7da55c8c233a5fa05d54597f8daa1abf34b
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2024-07-28 23:55:04 +0700

    try to combine navbar 1 and navbar 2 into one file, but fail. will continue tmr

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar1 from './compnent/Navbar1';
 import Navbar2 from './compnent/Navbar2';
+import navbarContainer from './compnent/navbarcontainer';
 function App() {
   return (
     <div>
-      <Navbar1/>
-      <Navbar2/>
+      <navbarContainer/>
     </div>
     
   );
---
+++ src/compnent/navbarcontainer.js
@@ -0,0 +1,11 @@
+import Navbar1 from "./Navbar1";
+import Navbar2 from "./Navbar2";
+export default function navbarContainer(){
+    return(
+        <div>
+            <Navbar1/>
+            <div class="">here's the line</div>
+            <Navbar2/>
+        </div>
+    );
+}
