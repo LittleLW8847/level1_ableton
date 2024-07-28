2eacd0b274a29e7a548f82c398b658884991f08c
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2024-07-28 23:21:10 +0700

    start working on second navbar, but there's problem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar1 from './compnent/Navbar1';
+import Navbar2 from './compnent/Navbar2';
 function App() {
   return (
-    <Navbar1></Navbar1>
+    <Navbar1/>
+    <Navbar2/>
   );
 }
 
---
+++ src/compnent/Navbar2.js
@@ -0,0 +1,7 @@
+export default function Navbar2(){
+    return(
+        <nav>
+            <h1>this is navbar2</h1>
+        </nav>
+    );
+}
