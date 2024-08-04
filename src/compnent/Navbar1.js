aa755a9a586409af214e3a20c3c8f8ac9d844823
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2024-08-05 02:00:58 +0700

    remove redundant part

--- src/compnent/Navbar1.js
+++ src/compnent/Navbar1.js
@@ -1,7 +1,7 @@
 export default function Navbar1(){
     return(
         <nav class ="flex flex-row py-5 w-full">
-            <span class = "left flex flex-row justify-flex-end mr-auto w-max">
+            <span class = "left flex flex-row justify-flex-end w-max">
                 <svg role="img" aria-label="Logo Ableton" class="basis-16 ml-10" fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="45" height="21" viewBox="0 0 45 21">
                     <path d="M0 0h3v21H0zM6 0h3v21H6zM12 0h3v21h-3zM18 0h3v21h-3zM24 18h21v3H24zM24 12h21v3H24zM24 6h21v3H24zM24 0h21v3H24z">
                     </path>
