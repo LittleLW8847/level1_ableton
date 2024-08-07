313abf5e65ef834edd654b3a71b5df68ce36a074
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2024-08-07 22:30:04 +0700

    working on responsive design for navbar layout

--- src/compnent/Navbar1.js
+++ src/compnent/Navbar1.js
@@ -1,13 +1,13 @@
 export default function Navbar1(){
     return(
         <nav class ="flex flex-row py-5 w-full">
-            <span class = "left flex flex-row justify-flex-end w-1/2">
+            <span class = "left flex flex-row justify-flex-end w-1/2 ">
                 <svg role="img" aria-label="Logo Ableton" class="basis-16 ml-10" fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="45" height="21" viewBox="0 0 45 21">
                     <path d="M0 0h3v21H0zM6 0h3v21H6zM12 0h3v21h-3zM18 0h3v21h-3zM24 18h21v3H24zM24 12h21v3H24zM24 6h21v3H24zM24 0h21v3H24z">
                     </path>
                 </svg>
 
-                <ul class="container flex flex-row pl-5 pr-1 justify-evenly"> 
+                <ul class="container flex flex-row pl-5 pr-1 justify-evenly sm:hidden"> 
                 <li class="basis-auto">Live</li>
                 <li class="basis-auto">Push</li>
                 <li class="basis-auto">Note</li>
@@ -17,6 +17,8 @@ export default function Navbar1(){
                 <li class="basis-auto">Help</li>
                 <li class="basis-auto">More +</li>
                 </ul>
+
+                <button class="hidden sm:block">Menu</button>
             </span>
             
 
