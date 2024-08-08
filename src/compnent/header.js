6e490a99a0128906d60fb743cb37773dbf7346b6
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2024-08-08 22:08:52 +0700

    cover pic added

--- src/compnent/header.js
+++ src/compnent/header.js
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
         <header class="w-fit">
-            <img src="../../resource/header.avif"></img>
+            <img src="https://ableton-production.imgix.net/about/header.jpg?auto=format&dpr=2&fit=crop&fm=jpg&h=362&ixjsv=1.1.3&q=50&w=1333" alt="cover-pic"></img>
         </header>
     );
 }
