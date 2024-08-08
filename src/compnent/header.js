53c3f40ea08904c4f2add7110a04773e67e1008e
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2024-08-08 23:43:57 +0700

    woring on header section layout. i can't get the text to the upper layer from cover pic

--- src/compnent/header.js
+++ src/compnent/header.js
@@ -1,7 +1,10 @@
 export default function Header(){
     return(
         <header class="w-fit">
-            <img src="https://ableton-production.imgix.net/about/header.jpg?auto=format&dpr=2&fit=crop&fm=jpg&h=362&ixjsv=1.1.3&q=50&w=1333" alt="cover-pic"></img>
+            <img src="https://ableton-production.imgix.net/about/header.jpg?auto=format&dpr=2&fit=crop&fm=jpg&h=362&ixjsv=1.1.3&q=50&w=1333" alt="cover-pic" class="z-0"></img>
+            
+            <h1 class="text-orange-600 z-10 absolute top-50 left-50">Ableton</h1>
+                
         </header>
     );
 }
