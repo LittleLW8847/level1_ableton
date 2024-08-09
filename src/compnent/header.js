f526b180a2e81173e8ee55eeee00ec7acfba160a
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2024-08-09 22:49:16 +0700

    get name into the middle of the header. i think.

--- src/compnent/header.js
+++ src/compnent/header.js
@@ -3,7 +3,7 @@ export default function Header(){
         <header class="w-fit">
             <img src="https://ableton-production.imgix.net/about/header.jpg?auto=format&dpr=2&fit=crop&fm=jpg&h=362&ixjsv=1.1.3&q=50&w=1333" alt="cover-pic" class="z-0"></img>
             
-            <h1 class="text-orange-600 z-10 absolute top-50 left-50">Ableton</h1>
+            <h1 class="text-orange-600 z-10 relative inset-x-1/2 inset-y-1/2">Ableton</h1>
                 
         </header>
     );
