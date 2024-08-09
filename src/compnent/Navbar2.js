643d201ac987785ac86e4ceb0177ab01b44826b9
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2024-08-09 23:08:23 +0700

    new problem discovered. the cover picture is too small(not enough height compare to original) will try to find remedy

--- src/compnent/Navbar2.js
+++ src/compnent/Navbar2.js
@@ -1,6 +1,6 @@
 export default function Navbar2(){
     return(
-        <nav class="flex flex-row">
+        <nav class="flex flex-row mb-5">
             <ul class="flex flex-row mx-10">
                 <li class="basis-10 mr-2">About</li>
                 <li class="basis-10 mx-3.5">Jobs</li>
--- src/compnent/header.js
+++ src/compnent/header.js
@@ -1,9 +1,11 @@
 export default function Header(){
     return(
-        <header class="w-fit">
-            <img src="https://ableton-production.imgix.net/about/header.jpg?auto=format&dpr=2&fit=crop&fm=jpg&h=362&ixjsv=1.1.3&q=50&w=1333" alt="cover-pic" class="z-0"></img>
+        <header class="w-screen block max-h-fit">
             
-            <h1 class="text-orange-600 z-10 relative inset-x-1/2 inset-y-1/2">Ableton</h1>
+            <img src="https://ableton-production.imgix.net/about/header.jpg?auto=format&dpr=2&fit=crop&fm=jpg&h=362&ixjsv=1.1.3&q=50&w=1333" alt="cover-pic" 
+            class="z-0 relative top-20 h-fit"></img>
+            
+            <h1 class="text-orange-600 z-10 absolute inset-x-1/2 inset-y-1/2">Ableton</h1>
                 
         </header>
     );
