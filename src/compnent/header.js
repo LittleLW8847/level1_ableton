8a58b96793d9d6aa68cc40473617ad60f5ae87d3
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2024-08-12 01:09:57 +0700

    that was a lie i can't figure out how to anchor the text to be in the middle of picture without using grid

--- src/compnent/header.js
+++ src/compnent/header.js
@@ -3,11 +3,9 @@ export default function Header(){
         <header class="w-screen block max-h-fit max-w-5xl">
             
             <img src="https://ableton-production.imgix.net/about/header.jpg?auto=format&dpr=2&fit=crop&fm=jpg&h=362&ixjsv=1.1.3&q=50&w=1333" alt="cover-pic" 
-            class="z-0 relative top-20 h-fit"></img>
+            class="z-0 relative h-fit"></img>
             
-            <h1 class="text-orange-600 z-1 relative inset-x-1/2 inset-y-1/2">Ableton</h1>
-            <h1>We make Live, Push, Note and Link — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.</h1>
-                
+            <h1 class="text-orange-600 z-1 absolute">Ableton</h1>                
         </header>
     );
 }
