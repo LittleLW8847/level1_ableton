c120cb3f4290b9f45cb8eb9010f161f2e226d422
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2024-08-15 00:18:27 +0700

    that's a correct format for the most part. now on to. responsive for these 2. i still have to figure things out with these

--- src/compnent/AboutText.js
+++ src/compnent/AboutText.js
@@ -1,9 +1,9 @@
 export default function AboutText(){
     return(
         <div class="body-text-container max-w-5xl">
-            <div class="body-text w-2/5 justify-center mx-auto">
-                <h1 class="">We make Live, Push, Note and Link — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.</h1>
-                <p></p>
+            <div class="body-text w-3/5 justify-center mx-auto">
+                <h1 class="w-9/12 mb-3">We make Live, Push, Note and Link — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.</h1>
+                <p class="w-11/12">Ableton was founded in 1999 and released the first version of Live in 2001. Our products are used by a community of dedicated musicians, sound designers, and artists from across the world.</p>
              </div>
 
         </div>
