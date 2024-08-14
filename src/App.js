9ccd947fc16a9fdbc3bc25736e93dc31e0baf64b
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2024-08-14 23:55:57 +0700

    progree, adding text to about page with "proper" format

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Navbar1 from './compnent/Navbar1';
 import Navbar2 from './compnent/Navbar2';
 import navbarContainer from './compnent/navbarcontainer';
 import Header from './compnent/header';
+import AboutText from './compnent/AboutText';
 function App() {
   return (
     <div class="ml-44">
       <Navbar1/>
       <Navbar2/>
       <Header/>
+      <AboutText/>
     </div>
     
   );
---
+++ src/compnent/AboutText.js
@@ -0,0 +1,11 @@
+export default function AboutText(){
+    return(
+        <div class="body-text-container max-w-5xl">
+            <div class="body-text w-2/5 justify-center mx-auto">
+                <h1 class="">We make Live, Push, Note and Link — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.</h1>
+                <p></p>
+             </div>
+
+        </div>
+    );
+}
