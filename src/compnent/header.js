f428b17979c65931be12f6bb6b699911c15703e1
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2024-08-15 00:23:36 +0700

    minor format change to fit with responsive designed.

--- src/compnent/header.js
+++ src/compnent/header.js
@@ -1,9 +1,9 @@
 export default function Header(){
     return(
-        <header class="w-screen block max-h-fit max-w-5xl">
+        <header class="w-screen block max-h-fit max-w-5xl justify-center">
             <div class="relative">
                 <img src="https://ableton-production.imgix.net/about/header.jpg?auto=format&dpr=2&fit=crop&fm=jpg&h=362&ixjsv=1.1.3&q=50&w=1333" alt="cover-pic" 
-            class="z-0"></img>
+            class="z-0 mx-5 mb-10"></img>
             
                 <h1 class="text-orange-600 z-1 absolute inset-1/2">Ableton</h1>          
             </div>
