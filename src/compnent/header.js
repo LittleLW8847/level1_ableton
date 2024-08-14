224fbf1a12d897864db2bbe4e04da784bfa5e38f
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2024-08-14 23:31:00 +0700

    Ohhhhh i see the light and understand the relation between position relative and absolute parent and child now

--- src/compnent/header.js
+++ src/compnent/header.js
@@ -1,11 +1,13 @@
 export default function Header(){
     return(
         <header class="w-screen block max-h-fit max-w-5xl">
+            <div class="relative">
+                <img src="https://ableton-production.imgix.net/about/header.jpg?auto=format&dpr=2&fit=crop&fm=jpg&h=362&ixjsv=1.1.3&q=50&w=1333" alt="cover-pic" 
+            class="z-0"></img>
             
-            <img src="https://ableton-production.imgix.net/about/header.jpg?auto=format&dpr=2&fit=crop&fm=jpg&h=362&ixjsv=1.1.3&q=50&w=1333" alt="cover-pic" 
-            class="z-0 relative h-fit"></img>
-            
-            <h1 class="text-orange-600 z-1 absolute">Ableton</h1>                
+                <h1 class="text-orange-600 z-1 absolute inset-1/2">Ableton</h1>          
+            </div>
+                  
         </header>
     );
 }
